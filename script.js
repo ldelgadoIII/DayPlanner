2e45cf4ed4ac238c603b66152174d3434f5a2d11
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-01-21 15:47:55 -0600

    added script link to jquery and connected to time block container

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 // DEPENDENCIES ===========================
 // connect to container to append time block
+let timeBlockContainer = $(".container");
+
 // connect to the save button for each individual time block
 
 // STARTING DATA ==========================
-// object thats stores time, description and save button
+// object thats stores time, current time, description and save button
 
 // FUNCTIONS ==============================
 // something to save the data within the time block
+// something to change the color of the text areas depending on the time of day
 
 // INITIALIZE =============================
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
