7fa0c810b612fe6bd319f247133691758a7f971f
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-01-21 16:06:12 -0600

    created an array to hold data and an append function that outputs one time-block

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 // DEPENDENCIES ===========================
 // connect to container to append time block
 let timeBlockContainer = $(".container");
-
 // connect to the save button for each individual time block
 
 // STARTING DATA ==========================
-// object thats stores time, current time, description and save button
+// An array to save data objects for time-blocks
+let timeBlocks = [];
+let timeCondition = "present";
 
 // FUNCTIONS ==============================
+// display time blocks
+
+let divTag = $('<div class="row time-block">');
+divTag.html(`<div class="hour col-md-1">9:00</div>
+<textarea class="description col-md-10 ${timeCondition}">Description</textarea>
+<button class="col-md-1 btn saveBtn">Save</button>`);
+timeBlockContainer.append(divTag);
+
 // something to save the data within the time block
 // something to change the color of the text areas depending on the time of day
 
