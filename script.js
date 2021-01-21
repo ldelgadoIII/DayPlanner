b24b2ad4c406a06519a9d6f5b4057b8c93e0ab20
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-01-21 15:27:53 -0600

    added comments to organize objectives

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-console.log(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+// DEPENDENCIES ===========================
+// connect to container to append time block
+// connect to the save button for each individual time block
+
+// STARTING DATA ==========================
+// object thats stores time, description and save button
+
+// FUNCTIONS ==============================
+// something to save the data within the time block
+
+// INITIALIZE =============================
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
